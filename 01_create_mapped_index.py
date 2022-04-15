17b09a016594c15a6328f9d2ec21fa24bb6be590
Switt Chawanwanidchai <switt.chaw@gmail.com>
2022-04-15 12:30:02 +0700

    initalise

---
+++ 01_create_mapped_index.py
@@ -0,0 +1,24 @@
+from elasticsearch import Elasticsearch
+
+# creating client 
+es = Elasticsearch("http://localhost:9200")
+
+# setting: one node
+# mapping properties are not required
+request_body = {
+    "settings" : {
+        "number_of_shards": 1,
+        "number_of_replicas": 1
+    },
+    'mappings': {
+        'properties': {
+            'process': {'type': 'text'},
+            'date': {'format': 'yyyy-MM-dd', 'type': 'date'},
+            'processing_time': {'type': 'double'},
+            'records': {'type': 'integer'},
+            'msg': {'type': 'text'}
+        }}
+}
+
+print("Creating 'example_index' index...")
+es.indices.create(index = 'etl_monitoring', body = request_body)
